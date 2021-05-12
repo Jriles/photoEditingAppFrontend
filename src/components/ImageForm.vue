ca4d6f6e8cd3e5b5f0780bc96c84e25c92af9d30
Jack Riley <jriley9000@gmail.com>
2021-05-12 07:16:30 -0700

    change to canvas position

--- src/components/ImageForm.vue
+++ src/components/ImageForm.vue
@@ -1023,7 +1023,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .navbar-offset{
-    margin-top: 40px !important;
+    margin-top: 50px !important;
   }
 
   .main-title{
